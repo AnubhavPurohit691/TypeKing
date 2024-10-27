8fed1caadc05fb2237b8bf7cbc9757e58729ffd0
Anubhav purohit <anubhavpurohitno1@gmail.com>
2024-10-28 01:23:41 +0530

    half done

---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import Firstpage from "./page/firstpage";
+
+export default function Home() {
+  return (
+    <div className="">
+      <Firstpage/>
+    </div>
+  );
+}
